31fa81ef2d6b7b8d7f82f24280f5a804e0e13aea
Stepamur <murashovssofficial@gmail.com>
2023-11-19 22:50:51 +0300

    Added more StackPush into main to find out if it works correctly

--- def_stack_main.cpp
+++ def_stack_main.cpp
@@ -10,10 +10,34 @@ int main ()
     StackPush (&pointer, 3);
     StackPush (&pointer, 4);
     StackPush (&pointer, 5);
-    int popped = StackPop (&pointer);
 
+    int popped = StackPop (&pointer);
     printf ("%d was popped \n \n", popped);
 
+    StackPrint (&pointer);
+
+    StackPush (&pointer, 6);
+    StackPush (&pointer, 7);
+    StackPush (&pointer, 8);
+    StackPush (&pointer, 9);
+    StackPush (&pointer, 10);
+
+    StackPrint (&pointer);
+
+    StackPush (&pointer, 11);
+    StackPush (&pointer, 12);
+    StackPush (&pointer, 13);
+    StackPush (&pointer, 14);
+    StackPush (&pointer, 15);
+
+    StackPrint (&pointer);
+
+    StackPush (&pointer, 16);
+    StackPush (&pointer, 17);
+    StackPush (&pointer, 18);
+    StackPush (&pointer, 19);
+    StackPush (&pointer, 20);
+    
     StackPrint (&pointer);
 
     StackDtor (&pointer);
